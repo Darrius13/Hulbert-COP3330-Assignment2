61f9d8d117a2b6de6636f475bb3d90a661c51adc
Darrius Hulbert <dhulbert41@gmail.com>
2021-06-12 20:44:23 -0400

    ....

--- src/main/java/oop/assignment2/Ex30/base/TimesTable.java
+++ src/main/java/oop/assignment2/Ex30/base/TimesTable.java
@@ -1,4 +1,18 @@
 package oop.assignment2.Ex30.base;
 
+
 public class TimesTable {
+    public static void main(String[] args)
+    {
+        for(int i = 1 ; i <= 12; i++)
+        {
+            for(int j = 1; j <= 12; j++)
+            {
+                System.out.print(i*j + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
